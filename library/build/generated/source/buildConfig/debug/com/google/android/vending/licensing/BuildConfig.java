94d15051ec9f9eb98ae711d0b4fd281f05c41048
Schmid Arnold <arnold@schmid-online.it>
2014-12-05 20:35:45 +0100

    commit gradle settings

--- library/build/generated/source/buildConfig/debug/com/google/android/vending/licensing/BuildConfig.java
+++ library/build/generated/source/buildConfig/debug/com/google/android/vending/licensing/BuildConfig.java
@@ -5,7 +5,7 @@ package com.google.android.vending.licensing;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String PACKAGE_NAME = "com.google.android.vending.licensing";
+  public static final String APPLICATION_ID = "com.google.android.vending.licensing";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
   public static final int VERSION_CODE = 2;
--- library/build/generated/source/buildConfig/release/com/google/android/vending/licensing/BuildConfig.java
+++ library/build/generated/source/buildConfig/release/com/google/android/vending/licensing/BuildConfig.java
@@ -5,7 +5,7 @@ package com.google.android.vending.licensing;
 
 public final class BuildConfig {
   public static final boolean DEBUG = false;
-  public static final String PACKAGE_NAME = "com.google.android.vending.licensing";
+  public static final String APPLICATION_ID = "com.google.android.vending.licensing";
   public static final String BUILD_TYPE = "release";
   public static final String FLAVOR = "";
   public static final int VERSION_CODE = 2;
